f90c53e11f2fe8f63c2a142740e838be667ee094
Ezedike-egwom Collins <ezedikecollins@gmail.com>
2025-01-25 22:48:35 +0100

    feat: implement onchain route for minting a token

---
+++ src/app/onchain/create-token/route.ts
@@ -0,0 +1,102 @@
+import { NextRequest, NextResponse } from "next/server";
+import {
+	createInitializeMintInstruction,
+	createInitializeNonTransferableMintInstruction,
+	ExtensionType,
+	getMintLen,
+	TOKEN_2022_PROGRAM_ID,
+} from "@solana/spl-token";
+import {
+	Connection,
+	Keypair,
+	PublicKey,
+	SystemProgram,
+	TransactionMessage,
+	VersionedTransaction,
+} from "@solana/web3.js";
+
+import { CLUSTER_URL } from "@/lib/utils";
+
+export async function POST(req: NextRequest) {
+	try {
+		const { name, symbol, description, image, authority, address } =
+			await req.json();
+		if (
+			!name?.trim() ||
+			!symbol?.trim() ||
+			!description?.trim() ||
+			!image?.trim() ||
+			!authority?.trim() ||
+			!address?.trim()
+		) {
+			return NextResponse.json(
+				{
+					error: "name, symbol, description, image, authority and address are required",
+				},
+				{ status: 400 }
+			);
+		}
+
+		let payer: PublicKey;
+		try {
+			payer = new PublicKey(address);
+		} catch (error: any) {
+			return NextResponse.json(
+				{ error: "Invalid address provided: not a valid public key" },
+				{ status: 500 }
+			);
+		}
+
+		const connection = new Connection(CLUSTER_URL);
+		const { blockhash } = await connection.getLatestBlockhash();
+
+		const mintKeypair = Keypair.generate();
+		const mint = mintKeypair.publicKey;
+		const decimals = 2;
+		const mintLen = getMintLen([ExtensionType.NonTransferable]);
+		const lamports = await connection.getMinimumBalanceForRentExemption(
+			mintLen
+		);
+
+		const createAccountIxn = SystemProgram.createAccount({
+			fromPubkey: payer,
+			newAccountPubkey: mint,
+			space: mintLen,
+			lamports,
+			programId: TOKEN_2022_PROGRAM_ID,
+		});
+
+		const initializeNonTransferableMintIxn =
+			createInitializeNonTransferableMintInstruction(
+				mint,
+				TOKEN_2022_PROGRAM_ID
+			);
+
+		const initializeMintIxn = createInitializeMintInstruction(
+			mint,
+			decimals,
+			payer,
+			null,
+			TOKEN_2022_PROGRAM_ID
+		);
+
+		const message = new TransactionMessage({
+			payerKey: payer,
+			recentBlockhash: blockhash,
+			instructions: [
+				createAccountIxn,
+				initializeNonTransferableMintIxn,
+				initializeMintIxn,
+			],
+		}).compileToV0Message();
+
+		const txn = new VersionedTransaction(message);
+
+		return NextResponse.json(txn, { status: 200 });
+	} catch (error: any) {
+		return NextResponse.json(
+			{ error: error.message ? error.message : error },
+			{ status: 500 }
+		);
+	}
+}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,31 @@
 import mongoose from "mongoose";
+import { clusterApiUrl, Connection } from "@solana/web3.js";
 
 export type ContextParamsType = { params: Promise<{ id: string }> };
+
+export const CLUSTER_URL = clusterApiUrl("devnet");
 
 export async function connectDB() {
 	if (mongoose.connection.readyState !== 1) {
 		await mongoose.connect(process.env.DATABASE_URL as string);
+	}
+}
+
+export async function isValidSignature(signature: string) {
+	try {
+		const connection = new Connection(CLUSTER_URL);
+		let status = await connection.getSignatureStatus(signature);
+		if (!status) throw new Error("Unknown signature status");
+		if (status.value?.confirmationStatus) {
+			if (
+				status.value.confirmationStatus != "confirmed" &&
+				status.value.confirmationStatus != "finalized"
+			) {
+				throw new Error("Unable to confirm the transaction");
+			}
+		}
+		return true;
+	} catch (err) {
+		throw err;
 	}
 }
