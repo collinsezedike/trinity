bc1fa7d852cce7b55967907b0f3f401be5f47dc6
Collins Ezedike-egwom <ezedikecollins@gmail.com>
2025-02-18 05:05:52 +0100

    feat: implement onchain token creation

--- src/app/onchain/create-token/route.ts
+++ src/app/onchain/create-token/route.ts
@@ -1,12 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 import {
-	createInitializeMintInstruction,
-	createInitializeNonTransferableMintInstruction,
-	ExtensionType,
-	getMintLen,
-	TOKEN_2022_PROGRAM_ID,
-} from "@solana/spl-token";
-import {
 	Connection,
 	Keypair,
 	PublicKey,
@@ -14,20 +7,35 @@ import {
 	TransactionMessage,
 	VersionedTransaction,
 } from "@solana/web3.js";
+import {
+	createInitializeMetadataPointerInstruction,
+	createInitializeMintInstruction,
+	createInitializeNonTransferableMintInstruction,
+	ExtensionType,
+	getMintLen,
+	LENGTH_SIZE,
+	TOKEN_2022_PROGRAM_ID,
+	TYPE_SIZE,
+} from "@solana/spl-token";
+import {
+	createInitializeInstruction,
+	createUpdateFieldInstruction,
+	pack,
+	TokenMetadata,
+} from "@solana/spl-token-metadata";
 
 import { CLUSTER_URL } from "@/lib/utils";
 
 export async function POST(req: NextRequest) {
 	try {
-		const { name, symbol, description, image, authority, address } =
+		const { name, symbol, description, image, authority } =
 			await req.json();
 		if (
 			!name?.trim() ||
 			!symbol?.trim() ||
 			!description?.trim() ||
 			!image?.trim() ||
-			!authority?.trim() ||
-			!address?.trim()
+			!authority?.trim()
 		) {
 			return NextResponse.json(
 				{
@@ -39,7 +47,7 @@ export async function POST(req: NextRequest) {
 
 		let payer: PublicKey;
 		try {
-			payer = new PublicKey(address);
+			payer = new PublicKey(authority);
 		} catch (error: any) {
 			return NextResponse.json(
 				{ error: "Invalid address provided: not a valid public key" },
@@ -47,15 +55,26 @@ export async function POST(req: NextRequest) {
 			);
 		}
 
-		const connection = new Connection(CLUSTER_URL);
-		const { blockhash } = await connection.getLatestBlockhash();
-
+		const connection = new Connection(CLUSTER_URL, "confirmed");
 		const mintKeypair = Keypair.generate();
 		const mint = mintKeypair.publicKey;
 		const decimals = 2;
-		const mintLen = getMintLen([ExtensionType.NonTransferable]);
+		const metaData: TokenMetadata = {
+			updateAuthority: payer,
+			mint,
+			name,
+			symbol,
+			uri: "",
+			additionalMetadata: [["description", description]],
+		};
+		const metadataExtension = TYPE_SIZE + LENGTH_SIZE;
+		const metadataLen = pack(metaData).length;
+		const mintLen = getMintLen([
+			ExtensionType.MetadataPointer,
+			ExtensionType.NonTransferable,
+		]);
 		const lamports = await connection.getMinimumBalanceForRentExemption(
-			mintLen
+			mintLen + metadataExtension + metadataLen
 		);
 
 		const createAccountIxn = SystemProgram.createAccount({
@@ -65,6 +84,14 @@ export async function POST(req: NextRequest) {
 			lamports,
 			programId: TOKEN_2022_PROGRAM_ID,
 		});
+
+		const initializeMetadataPointerIxn =
+			createInitializeMetadataPointerInstruction(
+				mint,
+				payer,
+				mint,
+				TOKEN_2022_PROGRAM_ID
+			);
 
 		const initializeNonTransferableMintIxn =
 			createInitializeNonTransferableMintInstruction(
@@ -80,13 +107,37 @@ export async function POST(req: NextRequest) {
 			TOKEN_2022_PROGRAM_ID
 		);
 
+		const initializeMetadataIxn = createInitializeInstruction({
+			programId: TOKEN_2022_PROGRAM_ID,
+			metadata: mint,
+			updateAuthority: payer,
+			mint: mint,
+			mintAuthority: payer,
+			name: metaData.name,
+			symbol: metaData.symbol,
+			uri: metaData.uri,
+		});
+
+		const updateFieldIxn = createUpdateFieldInstruction({
+			programId: TOKEN_2022_PROGRAM_ID,
+			metadata: mint,
+			updateAuthority: payer,
+			field: metaData.additionalMetadata[0][0],
+			value: metaData.additionalMetadata[0][1],
+		});
+
+		const { blockhash } = await connection.getLatestBlockhash();
+
 		const message = new TransactionMessage({
 			payerKey: payer,
 			recentBlockhash: blockhash,
 			instructions: [
 				createAccountIxn,
+				initializeMetadataPointerIxn,
 				initializeNonTransferableMintIxn,
 				initializeMintIxn,
+				initializeMetadataIxn,
+				updateFieldIxn,
 			],
 		}).compileToV0Message();
 
